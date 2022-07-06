e8c18aee1d807237edc4a02b7099c8a55db8d1e4
erick macias <103471146+lt0000er@users.noreply.github.com>
2022-07-05 22:42:57 -0500

    ejercicios propuestos

---
+++ tarea1.2.js
@@ -0,0 +1,58 @@
+//Replica el comportamiento de los siguientes ciclos for utilizando ciclos while
+
+
+for (let i = 0; i < 5; i++) {
+     console.log("El valor de i es: " + i);
+     }
+     //respuesta
+     var i = 0;
+     while(i < 5){
+       console.log(`El valor de i es ${i}`);
+       i++
+     }
+
+     
+     for (let i = 10; i >= 2; i--) { 
+    console.log("El valor de i es: " + i);
+    numero ++;
+     }
+
+     /// respuesta 
+     var i = 10;
+     while(i >= 2){
+       console.log(`El valor de i es ${i}`);
+       i--
+     }
+    }
+
+    //Escribe un código en JavaScript que le pregunte a los usuarios cuánto es 2 + 2 
+    //Si responden bien, mostramos un mensaje de felicitaciones, pero si responden mal, volvemos a empezar.
+    //Pista: puedes usar la función prompt de JavaScript.
+
+    var question = prompt("2 + 2 es igual a?");
+    if (Number(question) === 4){
+      console.log(`Felicitaciones`);
+    }else{
+      console.log(`Perdon, volvemos a empezar :(`);
+    }
+
+    ///Tienes un array de productos con los siguientes atributos:
+     //name 
+     //price 
+    //stock 
+     
+     var misProductos  = {Name: "arroz", price: 43, stock: 10};
+            while (prodcut = > 12%);
+            console.log( "el valor del iva es ${12%}" );
+
+     function allElements(ofThisProductos){
+        let productosValues = Productos.values(misProductos)
+        productosValues.forEach(item => iva); {
+          console.log(item)
+        })
+      }
+      
+      allElements(misProductos);
+
+ 
+   
